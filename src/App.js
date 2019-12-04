101c69a7af770d783f73a1489098a07d80f11e32
jafiecht <jafiecht@purdue.edu>
2019-12-03 16:02:16 -0800

    Timeline roughly done. Needs tooltip formatted.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import { ScatterChart, XAxis, YAxis, Scatter, Tooltip, ResponsiveContainer } from 'recharts';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -17,10 +18,14 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  dates: {
+    margin: theme.spacing(5),
+  },
 }));
   
 var selectedDates = [];
 var stringDates = [];
+var timelineData = [];
 
 function App() {
 
@@ -42,22 +47,36 @@ function App() {
     if (selectedDates.length < 11) {
       //Create an array of YYYY-MM-DD strings for display
       stringDates = selectedDates.map(dateObject => dateObject.toISOString().split('T')[0]);
+      timelineData = selectedDates.map(dateObject => {
+        var container = {};
+        container["date"] = dateObject.toISOString().split('T')[0];
+        container["timestamp"] = Date.parse(dateObject);
+        container["dummyXValue"] = 0;
+        return container;
+      });
     } else {
       selectedDates.shift()
       stringDates = selectedDates.map(dateObject => dateObject.toISOString().split('T')[0]);
+      timelineData = selectedDates.map(dateObject => {
+        var container = {};
+        container["date"] = dateObject.toISOString().split('T')[0];
+        container["timestamp"] = Date.parse(dateObject);
+        container["dummyXValue"] = 0;
+        return container;
+      });
     }
-
-
+    
+    console.log(timelineData);
 
   };  
 
   return (
     <Paper className={classes.root}>
       <Typography variant="h4">
-        Custom Date Chart
+        Custom Timeline
       </Typography>
       <Typography variant="h6">
-        Select up to 10 dates for display  
+        Select up to 10 dates  
       </Typography>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <KeyboardDatePicker
@@ -72,12 +91,30 @@ function App() {
           }}
         />
       </MuiPickersUtilsProvider>
-      <Typography>
+      <Typography className={classes.dates}>
         {stringDates.join(",  ")}
       </Typography>
+      <ResponsiveContainer height={40}>
+        <ScatterChart>
+          <XAxis 
+            dataKey="timestamp" 
+            domain={['dataMin', 'dataMax']}
+            type='number'
+            tick={false}
+          />
+          <YAxis 
+            dataKey="dummyXValue" 
+            type='number'
+            hide={true}
+          />
+          <Scatter data={timelineData}/>
+          <Tooltip cursor={false} />
+        </ScatterChart>
+      </ResponsiveContainer>
     </Paper>      
   );
 }
 
 export default App;
 
+            //domain={['dataMin'-3600000*24, 'dataMax'+3600000*24]}
