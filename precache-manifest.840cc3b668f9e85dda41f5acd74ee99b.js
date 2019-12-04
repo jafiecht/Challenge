824718ccdb69221daf9f5b1d392bc98764262e64
jafiecht <jafiecht@purdue.edu>
2019-12-03 17:26:30 -0800

    Updates

---
+++ precache-manifest.840cc3b668f9e85dda41f5acd74ee99b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33e4b72e7bfe789abe50fd175b7829e",
+    "url": "/Challenge/index.html"
+  },
+  {
+    "revision": "d2ccb154c88b66876daa",
+    "url": "/Challenge/static/js/2.ee3a0a7f.chunk.js"
+  },
+  {
+    "revision": "950da8fd16fdcdf583b9",
+    "url": "/Challenge/static/js/main.4275cdbb.chunk.js"
+  },
+  {
+    "revision": "6f38af1b1bf1d9b4c065",
+    "url": "/Challenge/static/js/runtime-main.54dfe16f.js"
+  }
+]);
